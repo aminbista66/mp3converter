7b834ef68505995327fbe0fbc2f5d5705af49578
Amin Bista <aminbista6666@gmail.com>
2024-03-16 06:54:37 +0545

    feat: download file stream

--- src/gateway/main.py
+++ src/gateway/main.py
@@ -4,8 +4,10 @@ from typing import Annotated
 from .auth_svc import access
 from fastapi.exceptions import HTTPException
 from .config import load_env
-from .utils import upload
+from .utils import upload, download
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+
 
 app = FastAPI(on_startup=[load_env])
 security = HTTPBasic()
@@ -20,11 +22,12 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.post("/login")
 def login(
@@ -33,8 +36,9 @@ def login(
     token, err = access.get_token(credentials)
     if err:
         return HTTPException(detail=err, status_code=403)
-    
+
     return token
+
 
 @app.post("/convert")
 def convert(req: Request, file: UploadFile | None = None):
@@ -42,22 +46,31 @@ def convert(req: Request, file: UploadFile | None = None):
         return {"message": "No file uploded"}
 
     token = req.headers["Authorization"]
-    verified, err = access.verify_token(token.split(' ')[1])
+    verified, err = access.verify_token(token.split(" ")[1])
 
     if err:
         raise HTTPException(detail=err, status_code=403)
 
     if not verified:
         raise HTTPException(detail="Invalid credentials", status_code=403)
-    
+
     user, err = access.get_email(token)
 
     if err:
         raise HTTPException(detail=err, status_code=403)
-    
+
     file_id, err = upload.upload_file(file, user)
-    
+
     if err:
         raise HTTPException(detail=err, status_code=403)
-    
+
     return {"file_id": file_id}
+
+
+@app.get("download/{mp3_id}")
+def download_mp3(mp3_id: str):
+    # Set response headers to stream the file
+    headers = {"Content-Disposition": f"attachment; filename="}
+    return StreamingResponse(
+        download.stream_file(mp3_id), media_type="audio/mpeg", headers=headers
+    )
---
+++ src/gateway/utils/download.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from gridfs import GridFS
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+client = MongoClient(os.environ.get("MONGO_URI"))
+mp3_db = client.get_database('mp3_db')
+fs_mp3s = GridFS(mp3_db)
+
+# Stream the file data back to the client
+def stream_file(file_id):
+    try:
+        with fs_mp3s.get(file_id) as file_stream:
+            for chunk in file_stream:
+                yield chunk
+    except Exception as err:
+        raise Exception(str(err))
